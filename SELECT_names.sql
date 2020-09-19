97a602c948cc8f88facbb8b6cb944d0a6dc46f54
gabriel ribeiro duarte <50786891+tenacidade@users.noreply.github.com>
2020-09-19 12:11:40 -0300

    SELECT names 1-14

--- SELECT_names.sql
+++ SELECT_names.sql
@@ -135,3 +135,15 @@ Find the capital and the name where the capital includes the name of the country
 SELECT capital, name
 FROM world
 WHERE capital LIKE concat('%', name, '%')
+
+--14#
+/*
+Find the capital and the name where the capital is an extension of name of the country.
+
+You should include Mexico City as it is longer than Mexico. You should not include Luxembourg as the capital is the same as the country.
+*/
+SELECT capital, name
+FROM world
+WHERE capital LIKE concat(name, '%') AND capital <> name;
+
+
