5fd1be2a2459b933bc07395fa95dc65a89821948
gabriel ribeiro duarte <50786891+tenacidade@users.noreply.github.com>
2020-09-19 16:56:00 -0300

    SELECT_within_SELECT 1-6

--- SELECT_within_SELECT.sql
+++ SELECT_within_SELECT.sql
@@ -71,5 +71,15 @@ SELECT name,
 FROM world                                                               --We want the name of these country (first SELECT) FROM world, WHERE continent = 'Europe'
 WHERE continent = 'Europe'
 
-
+--6#
+/*
+Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
+*/
+SELECT name
+FROM world
+WHERE gdp > ALL (SELECT gdp
+                 FROM world
+                 WHERE continent = 'Europe'
+                 AND gdp > 0)
+                   
 
