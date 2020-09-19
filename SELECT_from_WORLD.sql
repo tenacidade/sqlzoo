99c2c6cee1c1ee0af77fd48a70d9305239c2b8ae
gabriel ribeiro duarte <50786891+tenacidade@users.noreply.github.com>
2020-09-19 13:02:48 -0300

    SELECT_from_WORLD 1-9

--- SELECT_from_WORLD.sql
+++ SELECT_from_WORLD.sql
@@ -60,4 +60,28 @@ SELECT name, population, area
 FROM world
 WHERE population >=250000000 OR area >=3000000
 
+--8#
+/*
+Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area.
 
+Australia has a big area but a small population, it should be included.
+Indonesia has a big population but a small area, it should be included.
+China has a big population and big area, it should be excluded.
+United Kingdom has a small population and a small area, it should be excluded.
+*/
+SELECT name, population, area
+FROM world
+WHERE population >=250000000 XOR area >=3000000
+
+--9#
+/*
+Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places.
+
+For South America show population in millions and GDP in billions both to 2 decimal places.
+Millions and billions
+
+Divide by 1000000 (6 zeros) for millions. Divide by 1000000000 (9 zeros) for billions
+*/
+SELECT name, ROUND(population/1000000,2), ROUND(gdp/1000000000,2)
+FROM world
+WHERE continent = 'South America'
